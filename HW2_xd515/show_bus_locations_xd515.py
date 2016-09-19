b7f3d6ce0dd6f05e2dc817b134aae456965bfc03
xiaomeng <xd515@nyu.edu>
2016-09-19 13:29:15 -0400

    Update show_bus_locations_xd515.py

--- HW2_xd515/show_bus_locations_xd515.py
+++ HW2_xd515/show_bus_locations_xd515.py
@@ -1,1 +1,27 @@
+from __future__ import print_function
+import json
+import os
+import urllib2 
+import sys
 
+apikey = os.getenv("BUSAPIKEY")
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?\
+key=2a4a54c7-f386-4aaf-90a7-82fe5ed31bc4&VehicleMonitoringDetailLevel=calls&LineRef=B52"
+
+response = urllib2.urlopen(url)
+data = response.read()
+data = json.loads(data)
+print (data)
+
+data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][0]
+busline = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]\
+                ['VehicleActivity']
+busnumber = len(busline)
+
+print 'Bus line : %s' %sys.argv[2]
+print 'Number of Active Buses : %s' %busnumber
+
+for i in range (busnumber):
+    latitude = busline[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
+    longitude = busline[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
+    print ('Bus {} is at latitude {} and longitude {}'.format(i,latitude,longitude))
