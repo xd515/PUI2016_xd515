edad914eea4f29be0f3f0393d71a3d89022291bc
xiaomeng <xd515@nyu.edu>
2016-09-19 16:06:59 -0400

    Update show_bus_locations_xd515.py

--- HW2_xd515/show_bus_locations_xd515.py
+++ HW2_xd515/show_bus_locations_xd515.py
@@ -4,27 +4,24 @@ import os
 import urllib2 
 import sys
 
-apikey = os.getenv("BUSAPIKEY")
+apikey = sys.argv[1]
 bus = sys.argv[2]
-url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?\
-key=2a4a54c7-f386-4aaf-90a7-82fe5ed31bc4&VehicleMonitoringDetailLevel=calls&LineRef=B52"
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s" %(apikey, bus)
 
 response = urllib2.urlopen(url)
 data = response.read()
 data = json.loads(data)
-print (data)
 
 # Author: Fedhere
 
 data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][0]
-busline = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]\
-                ['VehicleActivity']
+busline = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 busnumber = len(busline)
 
-print 'Bus line : %s' %sys.argv[2]
-print 'Number of Active Buses : %s' %busnumber
+print ("Bus line : %s" %(sys.argv[2]))
+print ("Number of Active Buses : %s" %(busnumber))
 
-for i in range (0,len(busnumber)):
+for i in range (busnumber):
     latitude = busline[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
     longitude = busline[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
     print ('Bus {} is at latitude {} and longitude {}'.format(i,latitude,longitude))
