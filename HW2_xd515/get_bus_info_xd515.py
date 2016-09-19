bd4c2eb2d7a721a240af4ae7be5544f957d85f16
xiaomeng <xd515@nyu.edu>
2016-09-19 16:57:46 -0400

    Update get_bus_info_xd515.py

--- HW2_xd515/get_bus_info_xd515.py
+++ HW2_xd515/get_bus_info_xd515.py
@@ -2,23 +2,19 @@ from __future__ import print_function
 import json
 import os
 import urllib2 
+import csv
 import sys
 
-apikey = os.getenv("BUSAPIKEY")
+apikey = sys.argv[1]
 bus = sys.argv[2]
-url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?\
-key=2a4a54c7-f386-4aaf-90a7-82fe5ed31bc4&VehicleMonitoringDetailLevel=calls&LineRef=B52"
+url = "http://bustime.mta.info/api/siri/vehicle-monitoring.json?key=%s&VehicleMonitoringDetailLevel=calls&LineRef=%s" % (apikey, bus)
 
 response = urllib2.urlopen(url)
 data = response.read()
 data = json.loads(data)
-print (data)
-
 # Author: Fedhere
 
-data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity'][0]
-busline = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]\
-                ['VehicleActivity']
+busline = data['Siri']['ServiceDelivery']['VehicleMonitoringDelivery'][0]['VehicleActivity']
 busnumber = len(busline)
 
 with open(sys.argv[3], 'wb') as csvfile:
@@ -28,12 +24,3 @@ with open(sys.argv[3], 'wb') as csvfile:
         for i in range(busnumber):
             Lat = busline[i]['MonitoredVehicleJourney']['VehicleLocation']['Latitude']
             Lon = busline[i]['MonitoredVehicleJourney']['VehicleLocation']['Longitude']
-                
-            if busline[i]['MonitoredVehicleJourney']['OnwardCalls'] == {}:
-                StopName = "N/A"
-                StopStatus = "N/A"
-            else:
-                Stop_Name = busline[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['StopPointName']
-                Stop_Status = busline[i]['MonitoredVehicleJourney']['OnwardCalls']['OnwardCall'][0]['Extensions']['Distances']['PresentableDistance']
-                    
-            writer.writerow([Lat, Lon, Stop_Name, Stop_Status])
